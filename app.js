a70f5360de96d8f24f840fba9c13cb8c272da898
André Murakami <drehkenzo20@gmail.com>
2020-02-19 01:09:05 -0300

    rotas parametrizadas

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 let app = express();
 
-app.get('/', (req,res) => res.send("Ola mundo!"))
+app.get('/', (req,res) => res.send("Ola mundo!"));
+app.get('/produtos/:id?', (req,res) => {
+  let {id} = req.params;
+  res.send('eu tenho um produto com um id: ' + id);
+});
 
 app.listen(3000, () => {
-    console.log('servidor rodando');
+  console.log('servidor rodando');
 });
